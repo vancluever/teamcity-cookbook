4682e66a80a223c85aca344c717b2a070d3a9d33
Chris Marchesi <cmarchesi@paybyphone.com>
2016-01-04 16:53:29 -0800

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chrism@vancluevertech.com'
 license          'MIT'
 description      'Installs/Configures teamcity-cookbook'
 long_description 'Installs/Configures teamcity-cookbook'
-version          '1.1.0'
+version          '1.1.1'
 
 depends 'java'
 depends 'postgresql'
