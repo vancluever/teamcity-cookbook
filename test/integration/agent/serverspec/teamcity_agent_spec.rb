2f9b6f8c6cf6a2ef9931f685427f9f14e7bdf0bb
Chris Marchesi <cmarchesi@paybyphone.com>
2016-01-08 10:27:43 -0800

    Removing agent plugin test

--- test/integration/agent/serverspec/teamcity_agent_spec.rb
+++ test/integration/agent/serverspec/teamcity_agent_spec.rb
@@ -11,12 +11,3 @@ end
 describe service('teamcity-agent') do
   it { should be_running }
 end
-
-# NOTE: This test may be prone to a race condition as the ZIP should be
-# removed after the plugin is installed on the first-time agent startup.
-# However, the test currently seems to run before the ZIP is extracted.
-# If this turns out to NOT be the case, then this may change. I want to avoid
-# adding a sleep in or something like that for now.
-describe file('/opt/teamcity/buildAgent/plugins/idea.zip') do
-  it { should exist }
-end
